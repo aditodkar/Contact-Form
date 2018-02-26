c0788d7fbfc183f6030fa6320743a596d36820c2
Aditya Todkar <aditodkar@gmail.com>
2018-02-26 21:53:38 +0530

    Update contactform.js

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -4,15 +4,15 @@ import './contactform.css';
 
 	const config = {
 	apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_SENDER_ID
+        authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+        databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_SENDER_ID
 	};
 	firebase.initializeApp(config);
 
-  let contentref = firebase.database().ref('contact');
+        let contentref = firebase.database().ref('contact');
 
 class ContactForm extends Component {
   constructor(props){
